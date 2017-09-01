cb88b7f86b04217a3b943f6f01fd5335b739dfbb
radulfr96 <wados.russell70@gmail.com>
2017-09-01 15:12:11 +1000

    Add display sales

---
+++ DP2-UI/src/app/display-sales/display-sales.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'display-sales',
+  templateUrl: './display-sales.component.html'
+})
+
+export class DisplaySales {
+  title = 'display sales';
+  
+}
