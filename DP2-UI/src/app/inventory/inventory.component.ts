2932e32adc9db272e159bff73adee96f652c076f
radulfr96 <wados.russell70@gmail.com>
2017-09-01 14:57:48 +1000

    Fixed Inventory Naming

---
+++ DP2-UI/src/app/inventory/inventory.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'inventory',
+  templateUrl: './inventory.component.html'
+})
+
+export class Inventory {
+  title = 'inventory';
+  
+}
