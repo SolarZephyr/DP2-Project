f18f80c5ccee195bb81ae669c0b93d950d289fe1
AslanChadwick <2106736@student.swin.edu.au>
2017-10-07 14:29:14 +1100

    Sales predictions code refactor
    Sales predictions code refactor

--- DP2_API/models/PredictedSales.js
+++ DP2_API/models/PredictedSales.js
@@ -1,7 +1,74 @@
 var db = require('../dbconnection'); //reference of dbconnection.js  
 var PredictedSales = {  
-    getPredictedSales: function(sort, callback) {  
-        return db.query("select * from PredictedSales order by sort=?", [sort], callback);  
-    }
+    getPredictedSalesForItems: function(callback) {  
+			return db.query("SELECT Product.ID, Product.Name, Product.Type, Product.Price, Product.Stock, Transaction.Date as Date, Count(Sale.ProductID) AS Cnt FROM ((Sale Inner Join Product ON Sale.ProductId = Product.ID) Inner Join Transaction ON Sale.TransactionID = Transaction.ID) GROUP BY Product.Id, Transaction.Date ORDER BY Product.Id, Transaction.Date", function(err, result, fields)
+			{
+				var quresult = "";
+				var prevDate = result[0].Date;
+				prevDate.setDate(result[0].Date.getDate() - 1);
+				var currentID = result[0].ID;
+				var startIndex = 0;
+				
+				var xmean = 0.0;
+				var ymean = 0.0;
+				var counter = 0;
+				
+				var top = 0.0;
+				var bottom = 0.0;
+				
+				for(j = 0; j < result.length; j++)
+				{
+					if(currentID != result[j].ID)
+					{
+						xmean = (counter*(counter+1))/2;
+						ymean = ymean / (counter);
+						xmean = xmean / (counter);
+					
+						for(i = startIndex; i < j; i++)
+						{
+							top += ((i-xmean)*(result[i].Cnt-ymean));
+							bottom += ((i-xmean)*(i-xmean));
+						}
+					
+						quresult += "{\"ID\": \"" + result[j-1].ID + "\", \"Name\": \"" + result[j-1].Name + "\", \"Type\": \"" + result[j-1].Type + "\", \"Price\": \"" + result[j-1].Price + "\", \"Stock\": \"" + result[j-1].Stock + "\", \"Expected\": \"" + (top/bottom) + "\"},";
+					
+						xmean = 0.0;
+						ymean = 0.0;
+						counter = 0;
+						startIndex = j;
+						currentID = result[j].ID;
+					}
+					
+					prevDate.setDate(prevDate.getDate() + 1);
+					if(result[j].Date != prevDate)
+					{
+						do
+						{
+							prevDate.setDate(prevDate.getDate() + 1);
+							counter++;
+						}
+						while(result[j].Date > prevDate);
+						ymean += result[j].Cnt;
+					}
+					else
+					{	
+						counter++;
+						ymean += result[j].Cnt;
+					}
+					prevDate = result[j].Date;
+				}
+				xmean = (counter*(counter+1))/2;
+				ymean = ymean / (counter);
+				xmean = xmean / (counter);
+					
+				for(i = startIndex; i < j; i++)
+				{
+					top += ((i-xmean)*(result[i].Cnt-ymean));
+					bottom += ((i-xmean)*(i-xmean));
+				}
+				quresult += "{\"ID\": \"" + result[result.length-1].ID + "\", \"Name\": \"" + result[result.length-1].Name + "\", \"Type\": \"" + result[result.length-1].Type + "\", \"Price\": \"" + result[result.length-1].Price + "\", \"Stock\": \"" + result[result.length-1].Stock + "\", \"Expected\": \"" + (top/bottom) + "\"}";
+				callback(quresult);
+		});
+	}
 };  
 module.exports = PredictedSales; 
--- DP2_API/routes/PredictedSales.js
+++ DP2_API/routes/PredictedSales.js
@@ -1,16 +1,14 @@
 var express = require('express');  
 var router = express.Router();  
-var Transactions = require('../models/PredictedSales');  
-router.get('/:sort?', function(req, res) {   
-	if (req.params.id) {  
-		PredictedSales.getPredictedSales(req.params.id, function(err, rows) {  
-            if (err) {  
-                res.json(err);  
-            } else {  
-                res.json(rows);  
-            }  
-        });  
-	}
-}); 
+var PredictedSales = require('../models/PredictedSales');  
+router.get('/', function(req, res, next) {   
+		PredictedSales.getPredictedSalesForItems(function(err, rows) {  
+        if (err) {  
+            res.json(err);  
+        } else {  
+            res.json(rows); 
+        }  
+    });  
+});  
 
 module.exports = router; 
